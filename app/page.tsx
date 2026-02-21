751d19d0274835a94f63d060b40a3088b351f22f
Leader DEDI <farmertradecommunity@gmail.com>
2026-02-21 18:43:51 +0700

    fix caching production

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import { supabase } from "@/lib/supabase"
 import Link from "next/link"
-
+export const dynamic = "force-dynamic"
 export default async function Home() {
   const { data: traders } = await supabase
     .from("traders")
