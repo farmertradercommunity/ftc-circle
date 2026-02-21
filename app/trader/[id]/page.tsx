4894b65b705be0e145dac9b8b1277cd674ee2c35
Leader DEDI <farmertradecommunity@gmail.com>
2026-02-22 02:18:46 +0700

    joss bismillah terakhir

--- app/trader/[id]/page.tsx
+++ app/trader/[id]/page.tsx
@@ -4,9 +4,9 @@ import EquityChart from "./EquityChart"
 export default async function TraderDetail({
   params,
 }: {
-  params: { id: string }
+  params: Promise<{ id: string }>
 }) {
-  const id = params.id
+  const { id } = await params
 
   const { data: trader, error } = await supabase
     .from("traders")
